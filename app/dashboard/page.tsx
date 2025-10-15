fbf51f96fb0d322342f08b2ad5506820e5bcee4f
bl0ckchaindev <kryptodev7@gmail.com>
2025-10-16 07:37:13 +1000

    update withdrawToken function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,6 +12,6 @@ export default function DashboardPage() {
         <AnchorStakingDashboard />
         <Footer />
       </MotionWrapper>
-    // </ComingSoonWrapper>
+     </ComingSoonWrapper>
   )
 }
